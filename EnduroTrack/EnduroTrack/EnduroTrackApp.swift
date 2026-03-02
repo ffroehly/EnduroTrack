b0163c47e4d118d2152792ad6b5a473076740c17
ffroehly <fabrice.froehly@gmail.com>
2026-03-02 17:06:29 +0100

    Create project

---
+++ EnduroTrack/EnduroTrack/EnduroTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnduroTrackApp.swift
+//  EnduroTrack
+//
+//  Created by Fabrice FROEHLY on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnduroTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
